81755c5d1d5f842ff4314c4aa03a63d33932a568
Zachary Susswein <utb2@cdc.gov>
2024-08-31 15:03:27 +0000

    Document new functions

--- R/parameters.R
+++ R/parameters.R
@@ -200,6 +200,7 @@ read_interval_pmf <- function(path,
 }
 
 #' DuckDB date comparison fails if the dates are not in string format
+#' @noRd
 stringify_date <- function(date) {
   if (inherits(date, "Date")) {
     format(date, "%Y-%m-%d")
