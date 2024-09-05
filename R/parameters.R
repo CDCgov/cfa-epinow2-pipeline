39d7678e484bc1a73c390ba42526def009f86431
Zachary Susswein <utb2@cdc.gov>
2024-09-05 22:07:03 +0000

    Delete duplicate definition of `stringify_date()`

--- R/parameters.R
+++ R/parameters.R
@@ -228,11 +228,3 @@ read_interval_pmf <- function(path,
 
   return(pmf)
 }
-
-#' DuckDB date comparison fails if the dates are not in string format
-#' @noRd
-stringify_date <- function(date) {
-  if (inherits(date, "Date")) {
-    format(date, "%Y-%m-%d")
-  }
-}
