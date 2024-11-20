5e0a874ae7f383aad70e7cf9e5510adf3a015bcb
Zachary Susswein <utb2@cdc.gov>
2024-11-20 17:28:12 +0000

    Add test for missing stan keys error

---
+++ tests/testthat/helper-write_exclusion.R
@@ -0,0 +1,10 @@
+exclusions <- data.frame(
+  reference_date = as.Date("2023-01-07"),
+  report_date = as.Date("2023-10-28"),
+  state_abb = "test",
+  disease = "test"
+)
+con <- DBI::dbConnect(duckdb::duckdb())
+duckdb::duckdb_register(con, "exclusions", exclusions)
+DBI::dbExecute(con, "COPY (SELECT * FROM exclusions)
+                     TO 'tests/testthat/data/test_exclusions.csv'")
