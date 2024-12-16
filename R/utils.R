cc29ca200c799405142b4f7064b820bd7d448ded
Zachary Susswein <46581799+zsusswein@users.noreply.github.com>
2024-12-16 18:56:41 -0500

    Working azure download flow (#106)
    * Working azure download flow
    
    * Clean up some typos from translation
    
    * More debugging the flow
    
    * Run pipeline off ABS config
    
    * Download remote config in run
    
    * Add azure stuff
    
    * Fix an extra
    
    * Clean up build checks
    
    * Try rerunning after dropping cache
    
    * Temporarily drop config checks to match generator
    
    * Revert "Temporarily drop config checks to match generator"
    
    This reverts commit a5d57762e1df681fd48f96542a782938fa9c393b.
    
    * Working!!!
    
    * Working input/output dirs
    
    * Bump NEWS
    
    * Add doc for demo
    
    * Fix tests
    
    * Auto-detect tag
    
    * Typos
    
    * Move demo out of pkgdown's dir
    
    * Add working Batch implementation
    
    * Ignore secrets
    
    * Added prerequisites
    
    * Clean up a bit of the Azure stuff
    
    * Update Dockerfile
    
    * Remove non-standard uuid install
    
    * Update azure/job.py
    
    Co-authored-by: Nate McIntosh <NMcIntosh@cdc.gov>
    
    * One more cleanup from the big rebase
    
    * Mount dirs in `mnt`
    
    * Put everything in mnt for now
    
    * Clean up WARNING from undocumented arg
    
    * Ignore Azure stuff in R package
    
    * Add family for Azure
    
    * Pass image name and pool name
    
    This is a stopgap because we should be able to parse both from the
    same variable because everything is named by the branch. But for now,
    doing this.
    
    ---------
    
    Co-authored-by: kgostic <uep6@cdc.gov>
    Co-authored-by: Nate McIntosh <NMcIntosh@cdc.gov>

--- R/utils.R
+++ R/utils.R
@@ -21,6 +21,7 @@ check_file_exists <- function(data_path) {
 }
 
 #' If `x` is null or empty, return an empty string, otherwise `x`
+#' @noRd
 empty_str_if_non_existant <- function(x) {
   ifelse(rlang::is_empty(x), "", x)
 }
---
+++ azure/job.py
@@ -0,0 +1,96 @@
+import datetime
+import sys
+import os
+import uuid
+
+from azure.identity import DefaultAzureCredential
+from msrest.authentication import BasicTokenAuthentication
+from azure.storage.blob import BlobServiceClient
+from azure.batch import BatchServiceClient
+import azure.batch.models as batchmodels
+
+blob_account = os.environ["BLOB_ACCOUNT"]
+blob_url = f"https://{blob_account}.blob.core.windows.net"
+batch_account = os.environ["BATCH_ACCOUNT"]
+batch_url = f"https://{batch_account}.eastus.batch.azure.com"
+image_name = sys.argv[1]
+config_container = sys.argv[2]
+pool_id = sys.argv[3]
+# Re-use Azure Pool name unless otherwise specified
+job_id = sys.argv[4] if len(sys.argv) > 3 else pool_id
+
+if __name__ == "__main__":
+    # Authenticate with workaround because Batch is the one remaining
+    # service that doesn't yet support Azure auth flow v2 :) :)
+    # https://github.com/Azure/azure-sdk-for-python/issues/30468
+    credential_v2 = DefaultAzureCredential()
+    token = {"access_token": credential_v2.get_token("https://batch.core.windows.net/.default").token}
+    credential_v1 = BasicTokenAuthentication(token)
+
+    batch_client = BatchServiceClient(
+        credentials=credential_v1,
+        batch_url=batch_url
+     )
+
+    #############
+    # Set up job
+    job = batchmodels.JobAddParameter(
+        id=job_id,
+        pool_info=batchmodels.PoolInformation(pool_id=pool_id)
+    )
+
+    try:
+        batch_client.job.add(job)
+    except batchmodels.BatchErrorException as err:
+        if err.error.code != "JobExists":
+            raise
+        else:
+            print("Job already exists. Using job object")
+
+    ##########
+    # Get tasks
+    blob_service_client = BlobServiceClient(blob_url, credential_v2)
+    container_client = blob_service_client.get_container_client(container=config_container)
+    two_mins_ago = datetime.datetime.now(datetime.UTC) - datetime.timedelta(minutes=2)
+    task_configs: list[str] = [
+        b.name
+        for b in container_client.list_blobs()
+        if b.creation_time > two_mins_ago
+    ]
+    if len(task_configs) == 0:
+        raise ValueError("No tasks found")
+    else:
+        print(f"Creating {len(task_configs)} tasks in job {job_id} on pool {pool_id}")
+
+    ###########
+    # Set up task on job
+    registry = os.environ["AZURE_CONTAINER_REGISTRY"]
+    task_container_settings = batchmodels.TaskContainerSettings(
+        image_name=registry + '/cfa-epinow2-pipeline:test-' + image_name,
+        container_run_options='--rm --workdir /'
+    )
+    task_env_settings = [
+        batchmodels.EnvironmentSetting(name="az_tenant_id", value=os.environ["AZURE_TENANT_ID"]),
+        batchmodels.EnvironmentSetting(name="az_client_id", value=os.environ["AZURE_CLIENT_ID"]),
+        batchmodels.EnvironmentSetting(name="az_service_principal", value=os.environ["AZURE_CLIENT_SECRET"])
+    ]
+
+    # Run task at the admin level to be able to read/write to mounted drives
+    user_identity=batchmodels.UserIdentity(
+        auto_user=batchmodels.AutoUserSpecification(
+           scope=batchmodels.AutoUserScope.pool,
+           elevation_level=batchmodels.ElevationLevel.admin,
+        )
+    )
+
+    for config_path in task_configs:
+        command = f"Rscript -e \"CFAEpiNow2Pipeline::orchestrate_pipeline('{config_path}', config_container = '{config_container}', input_dir = '/mnt/input', output_dir = '/mnt/output', output_container = 'zs-test-pipeline-update')\""
+        task = batchmodels.TaskAddParameter(
+            id=str(uuid.uuid4()),
+            command_line=command,
+            container_settings=task_container_settings,
+            environment_settings=task_env_settings,
+            user_identity=user_identity
+        )
+
+        batch_client.task.add(job_id, task)
