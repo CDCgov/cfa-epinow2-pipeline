79a70f60b285626588ecff1358f688ca9a27e47c
George G. Vega Yon <g.vegayon@gmail.com>
2025-01-22 13:07:37 -0700

    Addressing comments by @zsusswein

---
+++ .github/scripts/cleanup-tags-on-pr-close.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#
+# Delete Batch Pools and associated jobs
+
+if [ "${#}" -ne 3 ]; then
+  echo "Usage: $0 <registry> <image> <tag>"
+  exit 1
+fi
+
+REGISTRY="$1"
+IMAGE="$2"
+TAG="$3"
+
+echo "Logging into Batch account"
+az batch account login \
+  --name "${ACCOUNT_NAME}" \
+  --resource-group "${RESOURCE_GROUP}"
+
+##########################
+# Delete container tags
+
+# Remove the image from the registry
+az acr repository delete \
+    --yes \
+    --name "${REGISTRY}" \
+    --image "${IMAGE}:${TAG}"
+
+az acr repository delete \
+    --yes \
+    --name "${REGISTRY}" \
+    --image "${IMAGE}:dependencies-${TAG}"
