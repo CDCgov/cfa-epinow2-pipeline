70979b90b6a982d4257618347a0e7077cb9b59ae
George G. Vega Yon <g.vegayon@gmail.com>
2025-01-29 19:01:35 +0000

    Deleting login step from script (already in action)

--- .github/scripts/cleanup-tags-on-pr-close.sh
+++ .github/scripts/cleanup-tags-on-pr-close.sh
@@ -2,21 +2,14 @@
 #
 # Delete container tags from Azure CR
 
-if [ "${#}" -ne 5 ]; then
-  echo "Usage: $0 <account name> <resource group> <registry> <image> <tag>"
+if [ "${#}" -ne 3 ]; then
+  echo "Usage: $0 <registry> <image> <tag>"
   exit 1
 fi
 
-ACCOUNT_NAME="$1"
-RESOURCE_GROUP="$2"
-REGISTRY="$3"
-IMAGE="$4"
-TAG="$5"
-
-echo "Logging into Batch account"
-az batch account login \
-  --name "${ACCOUNT_NAME}" \
-  --resource-group "${RESOURCE_GROUP}"
+REGISTRY="$1"
+IMAGE="$2"
+TAG="$3"
 
 ##########################
 # Delete container tags
