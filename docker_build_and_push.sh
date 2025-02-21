6f3defab16fefff8f599fd262fa341d6637b9312
Giovanni Rella <ap82@cdc.gov>
2025-02-21 10:50:02 -0500

    explicitly specifying docker-driver

--- docker_build_and_push.sh
+++ docker_build_and_push.sh
@@ -3,10 +3,10 @@ IMAGE=ghcr.io/cdcgov/cfa-epinow2-pipeline
 BUILDER=docker-container-driver-builder
 
 # create a builder. By default will use docker-container driver which allows cache export
-docker buildx create --name "$BUILDER" || true
+docker buildx create --name "$BUILDER" --driver=docker-container || true
 
-docker pull "$IMAGE:cache"
-echo "Pulled image - '$IMAGE:cache'"
+# docker pull "$IMAGE:cache"
+# echo "Pulled image - '$IMAGE:cache'"
 
 # use the cache tag and the latest tag for cache sources
 # in practice, the cache tag would instead be the name of a branch when adding
