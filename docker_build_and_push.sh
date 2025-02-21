7dbb1ea8639a09229a8185107764ae88ff4a6f5d
Giovanni Rella <ap82@cdc.gov>
2025-02-21 10:12:38 -0500

    Using unified dockerfile with buildx registry caching

---
+++ docker_build_and_push.sh
@@ -0,0 +1,17 @@
+TAG=$1
+IMAGE=ghcr.io/cdcgov/cfa-epinow2-pipeline
+BUILDER=docker-container-driver-builder
+
+# create a builder. By default will use docker-container driver which allows cache export
+docker buildx create --name "$BUILDER" || true
+
+# use the cache tag and the latest tag for cache sources
+# in practice, the cache tag would instead be the name of a branch when adding
+# commits to an open PR
+time docker buildx build --push -t "$IMAGE" \
+	--builder docker-container-driver-builder \
+	--cache-from "type=registry,ref=$IMAGE:$TAG" \
+	--cache-from "type=registry,ref=$IMAGE:cache" \
+	--cache-from "type=registry,ref=$IMAGE:latest" \
+	--cache-to "type=registry,ref=$IMAGE:$TAG,mode=max" \
+	-f Dockerfile.unified .
