9306646b787017574fafd4d7eea01f120100f9ca
aisha-cdc <an69@cdc.gov>
2025-03-07 18:52:22 +0000

    unified dockerfile

---
+++ .github/scripts/docker_build_and_push.sh
@@ -0,0 +1,13 @@
+MAGE="ghcr.io/cdcgov/$1"
+TAG=$2
+BUILDER=docker-container-driver-builder
+
+# create a builder with the docker-container driver to allow cache-export
+docker buildx create --name "$BUILDER" --driver=docker-container || true
+
+# use the registry cache for prior images of the same tag, or the 'latest' tag
+time docker buildx build --push -t "$IMAGE" \
+		--builder "$BUILDER" \
+			--cache-from "type=registry,ref=$IMAGE:$TAG" \
+				--cache-from "type=registry,ref=$IMAGE:latest" \
+					--cache-to "type=registry,ref=$IMAGE:$TAG,mode=max" \
