9d95b190fda5818bbc5aa9ba2417ee83ca2f9602
Nate McIntosh <NMcIntosh@cdc.gov>
2025-04-10 09:07:10 -0400

    Swap job.py from running in Docker to being an inline metadata script (#241)
    * use formatter
    
    * remove the docker setup for jobs
    
    * add news
    
    * fix logic mistake in checking for tasks
    
    thanks copilot for catching this one
    
    * Add pre-req for uv
    
    * pin versions
    
    I found these versions by running `uv lock --script azure/job.py`
    which created the file `azure/job.py.lock` that contains all of
    the version info (and more)

--- azure/job.py
+++ azure/job.py
@@ -1,16 +1,24 @@
-#!/usr/bin/env python3
-
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "azure-batch==14.2.0",
+#     "azure-identity==1.21.0",
+#     "azure-storage-blob==12.25.1",
+#     "msrest==0.7.1",
+# ]
+# ///
 import datetime
-import sys
 import os
-import uuid
+import sys
 import time
+import uuid
 
-from azure.identity import DefaultAzureCredential
 from msrest.authentication import BasicTokenAuthentication
-from azure.storage.blob import BlobServiceClient
-from azure.batch import BatchServiceClient
+
 import azure.batch.models as batchmodels
+from azure.batch import BatchServiceClient
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient
 
 blob_account = os.environ["BLOB_ACCOUNT"]
 blob_url = f"https://{blob_account}.blob.core.windows.net"
@@ -27,20 +35,20 @@ if __name__ == "__main__":
     # service that doesn't yet support Azure auth flow v2 :) :)
     # https://github.com/Azure/azure-sdk-for-python/issues/30468
     credential_v2 = DefaultAzureCredential()
-    token = {"access_token": credential_v2.get_token("https://batch.core.windows.net/.default").token}
+    token = {
+        "access_token": credential_v2.get_token(
+            "https://batch.core.windows.net/.default"
+        ).token
+    }
     credential_v1 = BasicTokenAuthentication(token)
 
-    batch_client = BatchServiceClient(
-        credentials=credential_v1,
-        batch_url=batch_url
-     )
+    batch_client = BatchServiceClient(credentials=credential_v1, batch_url=batch_url)
 
     #############
     # Set up job
     batch_job_id = pool_id
     job = batchmodels.JobAddParameter(
-        id=batch_job_id,
-        pool_info=batchmodels.PoolInformation(pool_id=pool_id)
+        id=batch_job_id, pool_info=batchmodels.PoolInformation(pool_id=pool_id)
     )
 
     try:
@@ -54,43 +62,50 @@ if __name__ == "__main__":
     ##########
     # Get tasks
     blob_service_client = BlobServiceClient(blob_url, credential_v2)
-    container_client = blob_service_client.get_container_client(container=config_container)
+    container_client = blob_service_client.get_container_client(
+        container=config_container
+    )
 
     start_time = datetime.datetime.now()
     end_time = start_time + datetime.timedelta(seconds=120)
     while datetime.datetime.now() < end_time:
         task_configs: list[str] = [
-            b.name
-            for b in container_client.list_blobs()
-            if job_id in b.name
+            b.name for b in container_client.list_blobs() if job_id in b.name
         ]
         if len(task_configs) == 0 and datetime.datetime.now() < end_time:
             print("No tasks currently found...Waiting 15 seconds to re-query")
             time.sleep(15)
         elif len(task_configs) > 0:
-            print(f"Creating {len(task_configs)} tasks in job {job_id} on pool {pool_id}")
+            print(
+                f"Creating {len(task_configs)} tasks in job {job_id} on pool {pool_id}"
+            )
             break
-        elif len(task_configs) == 0 and datetime.datetime.now() < end_time:
+        elif len(task_configs) == 0 and datetime.datetime.now() > end_time:
             raise ValueError("No tasks found")
 
     ###########
     # Set up tasks on job
     registry = os.environ["AZURE_CONTAINER_REGISTRY"]
     task_container_settings = batchmodels.TaskContainerSettings(
-        image_name=image_name,
-        container_run_options='--rm --workdir /'
+        image_name=image_name, container_run_options="--rm --workdir /"
     )
     task_env_settings = [
-        batchmodels.EnvironmentSetting(name="az_tenant_id", value=os.environ["AZURE_TENANT_ID"]),
-        batchmodels.EnvironmentSetting(name="az_client_id", value=os.environ["AZURE_CLIENT_ID"]),
-        batchmodels.EnvironmentSetting(name="az_service_principal", value=os.environ["AZURE_CLIENT_SECRET"])
+        batchmodels.EnvironmentSetting(
+            name="az_tenant_id", value=os.environ["AZURE_TENANT_ID"]
+        ),
+        batchmodels.EnvironmentSetting(
+            name="az_client_id", value=os.environ["AZURE_CLIENT_ID"]
+        ),
+        batchmodels.EnvironmentSetting(
+            name="az_service_principal", value=os.environ["AZURE_CLIENT_SECRET"]
+        ),
     ]
 
     # Run task at the admin level to be able to read/write to mounted drives
-    user_identity=batchmodels.UserIdentity(
+    user_identity = batchmodels.UserIdentity(
         auto_user=batchmodels.AutoUserSpecification(
-           scope=batchmodels.AutoUserScope.pool,
-           elevation_level=batchmodels.ElevationLevel.admin,
+            scope=batchmodels.AutoUserScope.pool,
+            elevation_level=batchmodels.ElevationLevel.admin,
         )
     )
 
@@ -101,7 +116,7 @@ if __name__ == "__main__":
             command_line=command,
             container_settings=task_container_settings,
             environment_settings=task_env_settings,
-            user_identity=user_identity
+            user_identity=user_identity,
         )
 
         batch_client.task.add(batch_job_id, task)
