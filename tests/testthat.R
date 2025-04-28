c93924d7ac8ed2655e60d3b26e73243d0a1528ee
micahwiesner67 <micahwiesner67@gmail.com>
2025-04-28 19:04:32 +0000

    moving suppress warning functions to test that global

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,6 +6,26 @@
 # * https://r-pkgs.org/testing-design.html#sec-tests-files-overview
 # * https://testthat.r-lib.org/articles/special-files.html
 
+suppress_ess_warning <- function(.f, pattern = NULL) {
+  if (is.null(pattern)) {
+    pattern <- "The ESS has been capped to avoid unstable"
+  }
+  force(.f) # ensure .f is evaluated once
+  function(...) {
+    withCallingHandlers(
+      .f(...),
+      warning = function(w) {
+        if (grepl(pattern, conditionMessage(w))) {
+          invokeRestart("muffleWarning")
+        }
+      }
+    )
+  }
+}
+
+orch_pipeline_clean <- suppress_ess_warning(orchestrate_pipeline)
+fit_model_clean     <- suppress_ess_warning(fit_model)
+
 library(testthat)
 library(CFAEpiNow2Pipeline)
 
