ec5de2bdf928597620b079184d25ae457ba04fde
Nate McIntosh <NMcIntosh@cdc.gov>
2025-05-20 15:07:46 -0400

    backfill config gen script complete

--- azure/generate_backfill_configs.py
+++ azure/generate_backfill_configs.py
@@ -17,8 +17,26 @@ from cfa_config_generator.utils.epinow2.driver_functions import generate_backfil
 
 
 def main(
-    state: Annotated[str, typer.Option(help="State(s)", show_default=False)],
-    disease: Annotated[str, typer.Option(help="Disease(s)", show_default=False)],
+    state: Annotated[
+        str,
+        typer.Option(
+            help=(
+                "State(s). Can be '*', 'all', a single state, or"
+                " multiple states, separated by commas."
+            ),
+            show_default=False,
+        ),
+    ],
+    disease: Annotated[
+        str,
+        typer.Option(
+            help=(
+                "Disease(s). Can be '*', 'all', a single disease, or"
+                " multiple diseases, separated by commas."
+            ),
+            show_default=False,
+        ),
+    ],
     str_report_dates: Annotated[
         str,
         typer.Option(
@@ -38,20 +56,47 @@ def main(
             show_default=False,
         ),
     ],
-    data_paths: Annotated[
-        str | None,
+    data_container: Annotated[
+        str,
         typer.Option(
             help=(
-                "A comma separated list of paths to the data. One path for each report date. "
-                "If the data is in blob, these should be the names of the blobs. "
-                "Cannot be used in conjunction with the --report-date-fstring option."
+                "The name of the blob storage container for input data. "
+                "Usually 'nssp-etl'"
             ),
+            show_default=False,
         ),
-    ] = None,
+    ],
+    backfill_name: Annotated[
+        str,
+        typer.Option(
+            help=(
+                "Name of the backfill run. This will be used to generate the job IDs for each"
+                "report date in the format `<backfill_name>_<report_date>`."
+            ),
+            show_default=False,
+        ),
+    ],
+    output_container: Annotated[
+        str,
+        typer.Option(
+            help=("Blob storage container to store model output."),
+            show_default=False,
+        ),
+    ],
+    str_as_of_dates: Annotated[
+        str,
+        typer.Option(
+            help=(
+                "The parameter as-of dates. Default is to match the report dates. "
+                "Otherwise, a comma separated list of dates in ISO format. "
+            )
+        ),
+    ] = "match_report_dates",
     report_date_fstring: Annotated[
         str | None,
         typer.Option(
             help=(
+                "Use this option over --str-data-paths most of the time. "
                 "A string representing the f-string format for data paths. "
                 "The '{}' section will be replaced with the report date "
                 "for each report date. This is most useful when pulling from the NSSP gold "
@@ -59,6 +104,25 @@ def main(
                 "for the report date 2025-01-01. "
                 "Cannot be used in conjunction with the --data-paths option."
             ),
+        ),
+    ] = None,
+    str_data_paths: Annotated[
+        str | None,
+        typer.Option(
+            help=(
+                "Comma separated paths to the data. One path for each report date. "
+                "If the data is in blob, these should be the names of the blobs. "
+                "Cannot be used in conjunction with the --report-date-fstring option."
+            ),
+        ),
+    ] = None,
+    task_exclusions: Annotated[
+        str | None,
+        typer.Option(
+            help=(
+                "Comma separated state:disease pairs."
+                " Will be applied to all report dates."
+            )
         ),
     ] = None,
 ) -> None:
@@ -69,24 +133,45 @@ def main(
     # This will handle cases like "2025-01-01, 2025-01-02 2025-01-03,2025-01-04".
     # Then parse each date string into a date object.
     report_dates: list[date] = [
-        date.fromisoformat(s2)
-        for s1 in str_report_dates.split(",")
-        for s2 in s1.split(" ")
-        if s2
+        date.fromisoformat(s.strip()) for s in str_report_dates.split(",")
     ]
 
     # Check if both data_paths and report_date_fstring are provided, and error out if so
-    if data_paths and report_date_fstring:
+    if str_data_paths and report_date_fstring:
         raise ValueError(
             "Cannot use both --data-paths and --report-date-fstring options at the same time."
         )
     # Check that at least one of data_paths or report_date_fstring is provided
-    if not data_paths and not report_date_fstring:
+    if (not str_data_paths) and (not report_date_fstring):
         raise ValueError(
             "Must provide either --data-paths or --report-date-fstring option."
         )
 
-    print(report_dates)
+    # If data_paths is provided, split on commas and remove empty strings
+    data_paths: list[str] = (
+        [s.strip() for s in str_data_paths.split(",")]
+        if str_data_paths and (not report_date_fstring)
+        else [report_date_fstring.format(report_date) for report_date in report_dates]  # type: ignore
+    )
+
+    as_of_dates: list[date] = (
+        [date.fromisoformat(s.strip()) for s in str_as_of_dates.split(",")]
+        if str_as_of_dates != "match_report_dates"
+        else report_dates
+    )
+
+    generate_backfill_config(
+        state=state,
+        disease=disease,
+        report_dates=report_dates,
+        reference_date_time_span=reference_date_time_span,
+        data_paths=data_paths,
+        data_container=data_container,
+        backfill_name=backfill_name,
+        as_of_dates=as_of_dates,
+        output_container=output_container,
+        task_exclusions=task_exclusions,
+    )
 
 
 if __name__ == "__main__":
