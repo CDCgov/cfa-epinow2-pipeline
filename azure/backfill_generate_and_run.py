c1fb9d4ae89192aa137925bee9863b5ac8345773
Nate McIntosh <NMcIntosh@cdc.gov>
2025-05-21 17:11:49 -0400

    move the job creation into the backfill config gen
    I decided to do this as it became clear that we needed
    to pass the job ids created during config generation
    to the job creation script. Either that happens with
    bash, or like this with python, which feels much safer.
    
    Have tested this in Batch, and things run!

--- azure/backfill_generate_and_run.py
+++ azure/backfill_generate_and_run.py
@@ -1,7 +1,11 @@
 # /// script
 # requires-python = ">=3.13"
 # dependencies = [
+#     "azure-batch==14.2.0",
+#     "azure-identity==1.21.0",
+#     "azure-storage-blob==12.25.1",
 #     "cfa-config-generator",
+#     "msrest==0.7.1",
 #     "typer",
 # ]
 #
@@ -9,11 +13,19 @@
 # cfa-config-generator = { git = "https://github.com/cdcgov/cfa-config-generator" }
 # ///
 
+import os
+import uuid
 from datetime import date
 from typing import Annotated
 
 import typer
 from cfa_config_generator.utils.epinow2.driver_functions import generate_backfill_config
+from msrest.authentication import BasicTokenAuthentication
+
+import azure.batch.models as batchmodels
+from azure.batch import BatchServiceClient
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient
 
 
 def main(
@@ -125,9 +137,34 @@ def main(
             )
         ),
     ] = None,
+    config_only: Annotated[
+        bool, typer.Option(help="Only generate config files.")
+    ] = False,
+    image_name: Annotated[
+        str | None,
+        typer.Option(
+            help=(
+                "The name of the container image (and tag) to use for the job. "
+                "This should be in the format 'registry/image:tag'."
+            ),
+            show_default=False,
+        ),
+    ] = None,
+    pool_id: Annotated[
+        str | None,
+        typer.Option(
+            help="The name of the pool to use for the job.",
+            show_default=False,
+        ),
+    ] = None,
+    config_container: Annotated[
+        str,
+        typer.Option(help="The name of the config file storage container"),
+    ] = "rt-epinow2-config",
 ) -> None:
     """
-    Generate and upload config files for the EpiNow2 pipeline.
+    Generate and upload config files for the EpiNow2 pipeline. Then, if asked for, kick
+    off those tasks in Azure Batch.
     """
     # Split on commas and spaces, and remove empty strings
     # This will handle cases like "2025-01-01, 2025-01-02 2025-01-03,2025-01-04".
@@ -145,6 +182,12 @@ def main(
     if (not str_data_paths) and (not data_paths_template):
         raise ValueError(
             "Must provide either --data-paths or --report-date-fstring option."
+        )
+
+    # Make sure the image and pool id are provided if config_only is False
+    if (config_only is False) and ((image_name is None) or (pool_id is None)):
+        raise ValueError(
+            "Must provide --image-name and --data-container options if not config_only."
         )
 
     # If data_paths is provided, split on commas and remove empty strings
@@ -175,6 +218,106 @@ def main(
         output_container=output_container,
         task_exclusions=task_exclusions,
     )
+
+    if config_only:
+        print("Config files generated. Exiting.")
+        return
+
+    # Explicitly case pool_id and image_name to str to make the type checker happy
+    pool_id = str(pool_id)
+    image_name = str(image_name)
+
+    # Kick off the tasks in Azure Batch
+    blob_account = os.environ["BLOB_ACCOUNT"]
+    blob_url = f"https://{blob_account}.blob.core.windows.net"
+    batch_account = os.environ["BATCH_ACCOUNT"]
+    batch_url = f"https://{batch_account}.eastus.batch.azure.com"
+
+    # Authenticate with workaround because Batch is the one remaining
+    # service that doesn't yet support Azure auth flow v2 :) :)
+    # https://github.com/Azure/azure-sdk-for-python/issues/30468
+    credential_v2 = DefaultAzureCredential()
+    token = {
+        "access_token": credential_v2.get_token(
+            "https://batch.core.windows.net/.default"
+        ).token
+    }
+    credential_v1 = BasicTokenAuthentication(token)
+
+    batch_client = BatchServiceClient(credentials=credential_v1, batch_url=batch_url)
+
+    #############
+    # Set up job
+    batch_job_id = pool_id
+    job = batchmodels.JobAddParameter(
+        id=batch_job_id, pool_info=batchmodels.PoolInformation(pool_id=pool_id)
+    )
+
+    try:
+        batch_client.job.add(job)
+    except batchmodels.BatchErrorException as err:
+        if err.error.code != "JobExists":
+            raise
+        else:
+            print("Job already exists. Using job object")
+
+    ##########
+    # Get tasks
+    blob_service_client = BlobServiceClient(blob_url, credential_v2)
+    container_client = blob_service_client.get_container_client(
+        container=config_container
+    )
+
+    task_configs: list[str] = [
+        b
+        # We can pre-filter on the backfill name, as it makes up the first part of the
+        # job id.
+        for b in container_client.list_blob_names(name_starts_with=backfill_name)
+        if any(job_id in b for job_id in job_ids)
+    ]
+    if len(task_configs) > 0:
+        print(
+            f"Creating {len(task_configs)} tasks in backfill job {backfill_name} on pool {pool_id}"
+        )
+    elif len(task_configs) == 0:
+        raise ValueError("No tasks found")
+
+    ###########
+    # Set up tasks on job
+    task_container_settings = batchmodels.TaskContainerSettings(
+        image_name=image_name, container_run_options="--rm --workdir /"
+    )
+    task_env_settings = [
+        batchmodels.EnvironmentSetting(
+            name="az_tenant_id", value=os.environ["AZURE_TENANT_ID"]
+        ),
+        batchmodels.EnvironmentSetting(
+            name="az_client_id", value=os.environ["AZURE_CLIENT_ID"]
+        ),
+        batchmodels.EnvironmentSetting(
+            name="az_service_principal", value=os.environ["AZURE_CLIENT_SECRET"]
+        ),
+    ]
+
+    # Run task at the admin level to be able to read/write to mounted drives
+    user_identity = batchmodels.UserIdentity(
+        auto_user=batchmodels.AutoUserSpecification(
+            scope=batchmodels.AutoUserScope.pool,
+            elevation_level=batchmodels.ElevationLevel.admin,
+        )
+    )
+
+    for config_path in task_configs:
+        command = f"Rscript -e \"CFAEpiNow2Pipeline::orchestrate_pipeline('{config_path}', config_container = '{config_container}', input_dir = '/mnt/input', output_dir = '/mnt/output')\""
+        task = batchmodels.TaskAddParameter(
+            id=str(uuid.uuid4()),
+            command_line=command,
+            container_settings=task_container_settings,
+            environment_settings=task_env_settings,
+            user_identity=user_identity,
+        )
+
+        batch_client.task.add(batch_job_id, task)
 
 
 if __name__ == "__main__":
