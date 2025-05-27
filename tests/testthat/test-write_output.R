0e5bcb95b31af681906aba1128ec230ca2cfaa8d
Micah Wiesner <micahwiesner67@gmail.com>
2025-05-27 15:20:14 +0000

    removing extra model fitting in tests in favor of loading from file

--- tests/testthat/test-write_output.R
+++ tests/testthat/test-write_output.R
@@ -222,58 +222,7 @@ test_that("process_quantiles works as expected (rstan)", {
 
 test_that("process_quantiles works as expected (cmdstanr)", {
   # Load the sample fit object
-  # fit <- readRDS(test_path("data", "sample_fit_cmdstanr_v2.rds"))
-
-  parameters <- list(
-    generation_interval = sir_gt_pmf,
-    delay_interval = c(0.2, 0.8),
-    right_truncation = c(0.7, 0.3)
-  )
-  # Data -- 5 points only
-  data_path <- test_path("data", "test_data.parquet")
-  con <- DBI::dbConnect(duckdb::duckdb())
-  data <- DBI::dbGetQuery(con, "
-                         SELECT
-                           report_date,
-                           reference_date,
-                           disease,
-                           geo_value AS state_abb,
-                           value AS confirm
-                         FROM read_parquet(?)
-                         ORDER BY reference_date
-                         LIMIT 5
-                          ",
-    params = list(data_path)
-  )
-  DBI::dbDisconnect(con)
-  # Priors
-  priors <- list(
-    rt = list(
-      mean = 1,
-      sd = 0.2
-    ),
-    gp = list(
-      alpha_sd = 0.05
-    )
-  )
-  # Sampler
-  sampler_opts <- list(
-    cores = 1,
-    chains = 1,
-    adapt_delta = 0.8,
-    max_treedepth = 10,
-    iter_warmup = 25,
-    iter_sampling = 25
-  )
-
-  fit <- fit_model(
-    data = data,
-    parameters = parameters,
-    seed = 12345,
-    horizon = 0,
-    priors = priors,
-    sampler = c(backend = "cmdstanr", sampler_opts)
-  )
+  fit <- readRDS(test_path("data", "sample_fit_cmdstanr.rds"))
 
   # Run the function on the fit object
   result <- process_quantiles(
@@ -355,68 +304,8 @@ test_that("process_quantiles works as expected (cmdstanr)", {
 
 test_that("draws table same between cmdstanr and rstan", {
   # Load the sample fit object
-  # fit_cmdstanr <- readRDS(test_path("data", "sample_fit_cmdstanr_v2.rds"))
-  # fit_rstan <- readRDS(test_path("data", "sample_fit_rstan.rds"))
-
-  parameters <- list(
-    generation_interval = sir_gt_pmf,
-    delay_interval = c(0.2, 0.8),
-    right_truncation = c(0.7, 0.3)
-  )
-  # Data -- 5 points only
-  data_path <- test_path("data", "test_data.parquet")
-  con <- DBI::dbConnect(duckdb::duckdb())
-  data <- DBI::dbGetQuery(con, "
-                         SELECT
-                           report_date,
-                           reference_date,
-                           disease,
-                           geo_value AS state_abb,
-                           value AS confirm
-                         FROM read_parquet(?)
-                         ORDER BY reference_date
-                         LIMIT 5
-                          ",
-    params = list(data_path)
-  )
-  DBI::dbDisconnect(con)
-  # Priors
-  priors <- list(
-    rt = list(
-      mean = 1,
-      sd = 0.2
-    ),
-    gp = list(
-      alpha_sd = 0.05
-    )
-  )
-  # Sampler
-  sampler_opts <- list(
-    cores = 1,
-    chains = 1,
-    adapt_delta = 0.8,
-    max_treedepth = 10,
-    iter_warmup = 25,
-    iter_sampling = 25
-  )
-
-  fit_cmdstanr <- fit_model(
-    data = data,
-    parameters = parameters,
-    seed = 12345,
-    horizon = 0,
-    priors = priors,
-    sampler = c(backend = "cmdstanr", sampler_opts)
-  )
-
-  fit_rstan <- fit_model(
-    data = data,
-    parameters = parameters,
-    seed = 12345,
-    horizon = 0,
-    priors = priors,
-    sampler = c(backend = "rstan", sampler_opts)
-  )
+  fit_cmdstanr <- readRDS(test_path("data", "sample_fit_cmdstanr.rds"))
+  fit_rstan <- readRDS(test_path("data", "sample_fit_rstan.rds"))
 
   # Run the function on the fit object
   draws_list_cmdstanr <- extract_draws_from_fit(fit_cmdstanr)
