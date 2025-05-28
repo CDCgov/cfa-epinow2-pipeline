8c9d88847cffc32dadd4474ca6310ca4b5e6501d
Micah Wiesner <micahwiesner67@gmail.com>
2025-05-28 16:22:17 +0000

    globalizing env variables for test environment

--- tests/testthat/test-fit_model.R
+++ tests/testthat/test-fit_model.R
@@ -1,55 +1,12 @@
 test_that("Minimal model fit all params runs", {
-  # Parameters
-  parameters <- list(
-    generation_interval = sir_gt_pmf,
-    delay_interval = c(0.2, 0.8),
-    right_truncation = c(0.7, 0.3)
-  )
-  # Data -- 5 points only
-  data_path <- test_path("data", "test_data.parquet")
-  con <- DBI::dbConnect(duckdb::duckdb())
-  data <- DBI::dbGetQuery(con, "
-                         SELECT
-                           report_date,
-                           reference_date,
-                           disease,
-                           geo_value AS state_abb,
-                           value AS confirm
-                         FROM read_parquet(?)
-                         ORDER BY reference_date
-                         LIMIT 5
-                          ",
-    params = list(data_path)
-  )
-  DBI::dbDisconnect(con)
-  # Priors
-  priors <- list(
-    rt = list(
-      mean = 1,
-      sd = 0.2
-    ),
-    gp = list(
-      alpha_sd = 0.05
-    )
-  )
-  # Sampler
-  sampler_opts <- list(
-    backend = "cmdstanr",
-    cores = 1,
-    chains = 1,
-    adapt_delta = 0.8,
-    max_treedepth = 10,
-    iter_warmup = 25,
-    iter_sampling = 25
-  )
-
+  
   fit <- fit_model(
     data = data,
     parameters = parameters,
     seed = 12345,
     horizon = 0,
     priors = priors,
-    sampler = sampler_opts
+    sampler = c(backend = "cmdstanr", sampler_opts)
   )
 
   expect_s3_class(fit, "epinow")
@@ -57,47 +14,6 @@ test_that("Minimal model fit all params runs", {
 
 test_that("Minimal model fit same between rstan or cmdstanr backend", {
   # Parameters
-  parameters <- list(
-    generation_interval = sir_gt_pmf,
-    delay_interval = c(0.2, 0.8),
-    right_truncation = c(0.7, 0.3)
-  )
-  # Data -- 5 points only
-  data_path <- test_path("data", "test_data.parquet")
-  con <- DBI::dbConnect(duckdb::duckdb())
-  data <- DBI::dbGetQuery(con, "
-                         SELECT
-                           report_date,
-                           reference_date,
-                           disease,
-                           geo_value AS state_abb,
-                           value AS confirm
-                         FROM read_parquet(?)
-                         ORDER BY reference_date
-                         LIMIT 5
-                          ",
-    params = list(data_path)
-  )
-  DBI::dbDisconnect(con)
-  # Priors
-  priors <- list(
-    rt = list(
-      mean = 1,
-      sd = 0.2
-    ),
-    gp = list(
-      alpha_sd = 0.05
-    )
-  )
-  # Sampler
-  sampler_opts <- list(
-    cores = 1,
-    chains = 1,
-    adapt_delta = 0.8,
-    max_treedepth = 10,
-    iter_warmup = 25,
-    iter_sampling = 25
-  )
 
   cmdstanr <- fit_model(
     data = data,
