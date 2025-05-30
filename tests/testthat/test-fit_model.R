1cb180bc0b7ff9431ed848b1f8082b9af13f1981
micahwiesner67 <micahwiesner67@gmail.com>
2025-05-30 15:24:23 +0000

    refactoring unit tests to utilize global test environment from setup.R

--- tests/testthat/test-fit_model.R
+++ tests/testthat/test-fit_model.R
@@ -1,105 +1,15 @@
 test_that("Minimal model fit all params runs", {
-  # Parameters
-  parameters <- list(
-    generation_interval = sir_gt_pmf,
-    delay_interval = c(0.2, 0.8),
-    right_truncation = c(0.7, 0.3)
-  )
-  # Data -- 5 points only
-  data_path <- test_path("data", "test_data.parquet")
-  con <- DBI::dbConnect(duckdb::duckdb())
-  data <- DBI::dbGetQuery(
-    con,
-    "
-                         SELECT
-                           report_date,
-                           reference_date,
-                           disease,
-                           geo_value AS state_abb,
-                           value AS confirm
-                         FROM read_parquet(?)
-                         ORDER BY reference_date
-                         LIMIT 5
-                          ",
-    params = list(data_path)
-  )
-  DBI::dbDisconnect(con)
-  # Priors
-  priors <- list(
-    rt = list(
-      mean = 1,
-      sd = 0.2
-    ),
-    gp = list(
-      alpha_sd = 0.05
-    )
-  )
-  # Sampler
-  sampler_opts <- list(
-    cores = 1,
-    chains = 1,
-    adapt_delta = 0.8,
-    max_treedepth = 10,
-    iter_warmup = 25,
-    iter_sampling = 25
-  )
-
-  fit <- fit_model(
-    data = data,
-    parameters = parameters,
-    seed = 12345,
-    horizon = 0,
-    priors = priors,
-    sampler = sampler_opts
-  )
-
+  # fit object created in setup.R 
   expect_s3_class(fit, "epinow")
 })
 
 test_that("Minimal model fit with no right trunc or delay runs", {
+  # Data loaded in from setup.R
   # Parameters
   parameters <- list(
     generation_interval = sir_gt_pmf,
     delay_interval = NA,
     right_truncation = NA
-  )
-  # Data -- 5 points only
-  data_path <- test_path("data", "test_data.parquet")
-  con <- DBI::dbConnect(duckdb::duckdb())
-  data <- DBI::dbGetQuery(
-    con,
-    "
-                         SELECT
-                           report_date,
-                           reference_date,
-                           disease,
-                           geo_value AS state_abb,
-                           value AS confirm
-                         FROM read_parquet(?)
-                         ORDER BY reference_date
-                         LIMIT 5
-                          ",
-    params = list(data_path)
-  )
-  DBI::dbDisconnect(con)
-  # Priors
-  priors <- list(
-    rt = list(
-      mean = 1,
-      sd = 0.2
-    ),
-    gp = list(
-      alpha_sd = 0.05
-    )
-  )
-  # Sampler
-  sampler_opts <- list(
-    cores = 1,
-    chains = 1,
-    adapt_delta = 0.8,
-    max_treedepth = 10,
-    iter_warmup = 25,
-    iter_sampling = 25
   )
 
   fit <- fit_model(
@@ -120,40 +30,12 @@ test_that("Bad params w/ failing fit issues warning and returns NA", {
   # the Stan sampler but it terminates unexpectedly with an error, which is the
   # desired testing condition.
 
+  # Data loaded in from setup.R
   # Parameters
   parameters <- list(
     generation_interval = sir_gt_pmf,
     delay_interval = NA,
     right_truncation = NA
-  )
-  # Data -- 5 points only
-  data_path <- test_path("data", "test_data.parquet")
-  con <- DBI::dbConnect(duckdb::duckdb())
-  data <- DBI::dbGetQuery(
-    con,
-    "
-                         SELECT
-                           report_date,
-                           reference_date,
-                           disease,
-                           geo_value AS state_abb,
-                           value AS confirm
-                         FROM read_parquet(?)
-                         ORDER BY reference_date
-                         LIMIT 5
-                          ",
-    params = list(data_path)
-  )
-  DBI::dbDisconnect(con)
-  # Priors
-  priors <- list(
-    rt = list(
-      mean = 1,
-      sd = 0.2
-    ),
-    gp = list(
-      alpha_sd = 0.05
-    )
   )
   # Sampler
   sampler_opts <- list(
