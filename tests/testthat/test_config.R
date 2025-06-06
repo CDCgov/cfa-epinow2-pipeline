d817af02f11b047586688a7698fa7ca4f976f396
Zachary Susswein <utb2@cdc.gov>
2025-06-06 21:08:51 +0000

    Use proper namespacing

--- tests/testthat/test_config.R
+++ tests/testthat/test_config.R
@@ -61,37 +61,40 @@ make_config <- function() {
 
 test_that("Exclusions class can be constructed and properties set", {
   excl <- make_exclusions()
-  expect_s3_class(excl, "Exclusions")
+  expect_s3_class(excl, "CFAEpiNow2Pipeline::Exclusions")
   expect_equal(S7::prop(excl, "path"), "ex.csv")
   expect_null(S7::prop(excl, "blob_storage_container"))
 })
 
 test_that("Interval and subclasses can be constructed", {
-  expect_s3_class(make_gen_interval(), "GenerationInterval")
-  expect_s3_class(make_delay_interval(), "DelayInterval")
-  expect_s3_class(make_right_trunc(), "RightTruncation")
+  expect_s3_class(make_gen_interval(), "CFAEpiNow2Pipeline::GenerationInterval")
+  expect_s3_class(make_delay_interval(), "CFAEpiNow2Pipeline::DelayInterval")
+  expect_s3_class(make_right_trunc(), "CFAEpiNow2Pipeline::RightTruncation")
   intv <- Interval(path = "int.csv", blob_storage_container = NULL)
-  expect_s3_class(intv, "Interval")
+  expect_s3_class(intv, "CFAEpiNow2Pipeline::Interval")
 })
 
 test_that("Parameters class can be constructed", {
   params <- make_params()
-  expect_s3_class(params, "Parameters")
+  expect_s3_class(params, "CFAEpiNow2Pipeline::Parameters")
   expect_equal(S7::prop(params, "as_of_date"), "2024-01-01")
-  expect_s3_class(S7::prop(params, "generation_interval"), "GenerationInterval")
+  expect_s3_class(
+    S7::prop(params, "generation_interval"),
+    "CFAEpiNow2Pipeline::GenerationInterval"
+  )
 })
 
 test_that("Data class can be constructed", {
   d <- make_data()
-  expect_s3_class(d, "Data")
+  expect_s3_class(d, "CFAEpiNow2Pipeline::Data")
   expect_equal(S7::prop(d, "path"), "foo.parquet")
 })
 
 test_that("Config class can be constructed with nested objects", {
   cfg <- make_config()
-  expect_s3_class(cfg, "Config")
+  expect_s3_class(cfg, "CFAEpiNow2Pipeline::Config")
   expect_equal(S7::prop(cfg, "job_id"), "job1")
-  expect_s3_class(S7::prop(cfg, "data"), "Data")
+  expect_s3_class(S7::prop(cfg, "data"), "CFAEpiNow2Pipeline::Data")
 })
 
 test_that("read_json_into_config reads and constructs Config object", {
@@ -144,10 +147,10 @@ test_that("read_json_into_config reads and constructs Config object", {
   tf <- tempfile(fileext = ".json")
   jsonlite::write_json(cfg_list, tf, auto_unbox = TRUE)
   cfg <- read_json_into_config(tf, optional_fields = c("output_container"))
-  expect_s3_class(cfg, "Config")
+  expect_s3_class(cfg, "CFAEpiNow2Pipeline::Config")
   expect_equal(S7::prop(cfg, "job_id"), "job1")
-  expect_s3_class(S7::prop(cfg, "data"), "Data")
-  expect_s3_class(S7::prop(cfg, "parameters"), "Parameters")
-  expect_s3_class(S7::prop(cfg, "exclusions"), "Exclusions")
+  expect_s3_class(S7::prop(cfg, "data"), "CFAEpiNow2Pipeline::Data")
+  expect_s3_class(S7::prop(cfg, "parameters"), "CFAEpiNow2Pipeline::Parameters")
+  expect_s3_class(S7::prop(cfg, "exclusions"), "CFAEpiNow2Pipeline::Exclusions")
   unlink(tf)
 })
