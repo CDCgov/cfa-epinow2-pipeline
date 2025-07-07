542815899b4c48c00e5b72f2312d499ed4bdb7ed
Micah Wiesner <33739832+micahwiesner67@users.noreply.github.com>
2025-07-07 13:54:05 -0400

    Update test-pipeline.R

--- tests/testthat/test-pipeline.R
+++ tests/testthat/test-pipeline.R
@@ -1,11 +1,3 @@
-# This file is part of the standard setup for testthat.
-# It is recommended that you do not modify it.
-#
-# Where should you do additional test configuration?
-# Learn more about the roles of various files in:
-# * https://r-pkgs.org/testing-design.html#sec-tests-files-overview
-# * https://testthat.r-lib.org/articles/special-files.html
-
 # Suppress common warning in tests from model fit due to cmdstanr
 suppress_ess_warning <- function(.f, pattern = NULL) {
   if (is.null(pattern)) {
