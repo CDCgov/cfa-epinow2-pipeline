ba74796df41618ece44352e98e2aa2810dcba29b
Kingsley Iyawe <46824056+kingsleyiyawe@users.noreply.github.com>
2025-08-06 12:00:52 -0400

    Update azure/generate_configs.py
    Co-authored-by: Katie Gostic <uep6@cdc.gov>

--- azure/generate_configs.py
+++ azure/generate_configs.py
@@ -51,7 +51,11 @@ def main(
             help="""
             Minimum proportion of days of a facility must be actively reporting DDI
             counts during the modeling period. Must be a number between 0 and 1.
-            Default is 1.0, meaning all facilities must have been active every day.
+            Default is 0.94 (require active reporting for >=53 of 56 days in the training period).
+            We do not recommend using 1.00, as this will filter out large fractions of the data in some states,
+            because not all facilities report actively same-day (at lag 0). This default was chosen for correspondence
+            with API v1, and it includes data from most facilities, 
+            even if they do not actively report at lag 0 or drop out for a few days incidentally.
             """,
             show_default=True,
         ),
