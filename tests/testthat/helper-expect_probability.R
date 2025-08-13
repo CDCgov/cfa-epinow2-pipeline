5c6a0bf93f14323d247a65f3f2ce9340fa40e62c
Zachary Susswein <utb2@cdc.gov>
2025-08-13 19:30:47 +0000

    Use a less brittle diagnostic test
    Rather than test for strict equality, test for expected properties.
    This approach is more robust to changes in the model over time because
    it looks for the type and properties of output. We don't expect the
    exact diagnostic flags to remain the same in all settings.

---
+++ tests/testthat/helper-expect_probability.R
@@ -0,0 +1,11 @@
+#' Testthat helper to check `actual` is a probability
+#'
+#' @param actual A length one numeric vector between 0 and 1
+#' @return NULL invisibly or a testthat failure as a side effect
+expect_probability <- function(actual) {
+    testthat::expect_length(actual, 1)
+    testthat::expect_numeric(actual)
+    testthat::expect_lte(actual, 1)
+    testthat::expect_gte(actual, 0)
+    invisible(NULL)
+}
