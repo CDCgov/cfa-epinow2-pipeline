0a4c85d4e16d9bc03cfa4bcfb4ed989206bbfaed
Zachary Susswein <46581799+zsusswein@users.noreply.github.com>
2025-08-13 15:42:48 -0400

    Apply suggestion from @github-actions[bot]
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/testthat/helper-expect_probability.R
+++ tests/testthat/helper-expect_probability.R
@@ -3,9 +3,9 @@
 #' @param actual A length one numeric vector between 0 and 1
 #' @return NULL invisibly or a testthat failure as a side effect
 expect_probability <- function(actual) {
-    testthat::expect_length(actual, 1)
-    testthat::expect_type(actual, "numeric")
-    testthat::expect_lte(actual, 1)
-    testthat::expect_gte(actual, 0)
-    invisible(NULL)
+  testthat::expect_length(actual, 1)
+  testthat::expect_type(actual, "numeric")
+  testthat::expect_lte(actual, 1)
+  testthat::expect_gte(actual, 0)
+  invisible(NULL)
 }
